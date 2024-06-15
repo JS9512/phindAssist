26701e92d5b8e782ac35a0d0930fec104d097daf
JS9512 <118939761+JS9512@users.noreply.github.com>
2024-06-15 11:01:44 +0200

    Add files via upload

--- app/src/main/java/org/phindassist/unofficial/MainActivity.java
+++ app/src/main/java/org/phindassist/unofficial/MainActivity.java
@@ -1,15 +1,21 @@
 package org.phindassist.unofficial;
 
 import static android.webkit.WebView.HitTestResult.IMAGE_TYPE;
+import static android.webkit.WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE;
+
 import android.Manifest;
 import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
 import android.view.ContextMenu;
 import android.view.KeyEvent;
 import android.view.View;
@@ -18,24 +24,21 @@ import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.CookieManager;
 import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
-import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
-import android.webkit.ValueCallback;
-import android.net.Uri;
+
+import java.util.Objects;
 
 
 public class MainActivity extends Activity {
 
     private WebView chatWebView = null;
-    private WebSettings chatWebSettings = null;
     private CookieManager chatCookieManager = null;
     private final Context context = this;
-    private String TAG ="phindAssist";
-    private String urlToLoad = "https://phind.com/";
 
     private ValueCallback<Uri[]> mUploadMessage;
     private final static int FILE_CHOOSER_REQUEST_CODE = 1;
@@ -83,13 +86,12 @@ public class MainActivity extends Activity {
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                         requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
                     }
                 }
-                if (mUploadMessage!= null) {
+                if (mUploadMessage != null) {
                     mUploadMessage.onReceiveValue(null);
-                    mUploadMessage = null;
                 }
 
                 mUploadMessage = filePathCallback;
@@ -105,7 +107,7 @@ public class MainActivity extends Activity {
         chatWebView.setWebViewClient(new WebViewClient());
 
         //Set more options
-        chatWebSettings = chatWebView.getSettings();
+        WebSettings chatWebSettings = chatWebView.getSettings();
         //Enable some WebView features
         chatWebSettings.setJavaScriptEnabled(true);
         chatWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
@@ -119,7 +121,11 @@ public class MainActivity extends Activity {
         chatWebSettings.setSaveFormData(false);
         chatWebSettings.setGeolocationEnabled(false);
 
+        //Change the User-Agent
+        chatWebSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 12; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36");
+
         //Load Phind
+        String urlToLoad = "https://phind.com/";
         chatWebView.loadUrl(urlToLoad);
     }
 
@@ -132,31 +138,16 @@ public class MainActivity extends Activity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         //Credit (CC BY-SA 3.0): https://stackoverflow.com/a/6077173
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            switch (keyCode) {
-                case KeyEvent.KEYCODE_BACK:
-                    if (chatWebView.canGoBack() &&!chatWebView.getUrl().equals("about:blank")) {
-                        chatWebView.goBack();
-                    } else {
-                        finish();
-                    }
-                    return true;
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (chatWebView.canGoBack() && !Objects.equals(chatWebView.getUrl(), "about:blank")) {
+                    chatWebView.goBack();
+                } else {
+                    finish();
+                }
+                return true;
             }
         }
         return super.onKeyDown(keyCode, event);
-    }
-
-    public void resetChat()  {
-
-        chatWebView.clearFormData();
-        chatWebView.clearHistory();
-        chatWebView.clearMatches();
-        chatWebView.clearSslPreferences();
-        chatCookieManager.removeSessionCookie();
-        chatCookieManager.removeAllCookie();
-        CookieManager.getInstance().removeAllCookies(null);
-        CookieManager.getInstance().flush();
-        WebStorage.getInstance().deleteAllData();
-        chatWebView.loadUrl(urlToLoad);
     }
 
     @Override
@@ -166,9 +157,9 @@ public class MainActivity extends Activity {
             if (mUploadMessage == null) return;
             Uri[] result = null;
             if (resultCode == Activity.RESULT_OK) {
-                if (intent!= null) {
+                if (intent != null) {
                     String dataString = intent.getDataString();
-                    if (dataString!= null) {
+                    if (dataString != null) {
                         result = new Uri[]{Uri.parse(dataString)};
                     }
                 }
@@ -182,9 +173,23 @@ public class MainActivity extends Activity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
         super.onCreateContextMenu(menu, v, menuInfo);
         WebView.HitTestResult result = chatWebView.getHitTestResult();
-        if (result.getExtra()!= null) {
-            if (result.getType() == IMAGE_TYPE) {
-                String url = result.getExtra();
+        String url="";
+        if (result.getExtra() != null) {
+            if (result.getType() == SRC_IMAGE_ANCHOR_TYPE) {
+                // Create a background thread that has a Looper
+                HandlerThread handlerThread = new HandlerThread("HandlerThread");
+                handlerThread.start();
+                // Create a handler to execute tasks in the background thread.
+                Handler backgroundHandler = new Handler(handlerThread.getLooper());
+                Message msg = backgroundHandler.obtainMessage();
+                chatWebView.requestFocusNodeHref(msg);
+                url = (String) msg.getData().get("src");
+            }  else if (result.getType() == IMAGE_TYPE) {
+                url = result.getExtra();
+            }
+            if (url != null) {
+                Toast.makeText(this,getString(R.string.downloading),Toast.LENGTH_LONG).show();
+                String filename = URLUtil.guessFileName(url, null, null);
                 Uri source = Uri.parse(url);
                 DownloadManager.Request request = new DownloadManager.Request(source);
                 request.addRequestHeader("Cookie", CookieManager.getInstance().getCookie(url));
@@ -192,28 +197,12 @@ public class MainActivity extends Activity {
                 request.addRequestHeader("Accept-Language", "en-US,en;q=0.7,he;q=0.3");
                 request.addRequestHeader("Referer", url);
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
-                String filename = URLUtil.guessFileName(url, null, "image/jpeg");
                 request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
-                Toast.makeText(this,getString(R.string.download)+"\n"+filename, Toast.LENGTH_SHORT).show();
                 DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                assert dm!= null;
+                assert dm != null;
                 dm.enqueue(request);
             }
         }
-    }
-
-    public String modUserAgent(){
-
-        String newPrefix = "Mozilla/5.0 (X11; Linux "+ System.getProperty("os.arch") +")";
-
-        String newUserAgent=WebSettings.getDefaultUserAgent(context);
-        String prefix = newUserAgent.substring(0, newUserAgent.indexOf(")") + 1);
-        try {
-            newUserAgent=newUserAgent.replace(prefix,newPrefix);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return newUserAgent;
     }
 
 }
