e3c84084ce0cccb2710671788882c4eaa57048c8
JS9512 <118939761+JS9512@users.noreply.github.com>
2024-07-11 17:19:16 +0200

    Update MainActivity.java

--- app/src/main/java/org/phindassist/unofficial/MainActivity.java
+++ app/src/main/java/org/phindassist/unofficial/MainActivity.java
@@ -1,83 +1,43 @@
 package org.phindassist.unofficial;
 
-import static android.webkit.WebView.HitTestResult.IMAGE_TYPE;
-import static android.webkit.WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE;
-
-import android.Manifest;
 import android.app.Activity;
 import android.app.DownloadManager;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Message;
-import android.view.ContextMenu;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
-import android.webkit.CookieManager;
-import android.webkit.URLUtil;
-import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-import java.util.Objects;
-
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
-    private WebView chatWebView = null;
-    private CookieManager chatCookieManager = null;
+    private WebView chatWebView;
+    private CookieManager chatCookieManager;
     private final Context context = this;
 
     private ValueCallback<Uri[]> mUploadMessage;
     private final static int FILE_CHOOSER_REQUEST_CODE = 1;
 
     @Override
-    protected void onPause() {
-        if (chatCookieManager!=null) chatCookieManager.flush();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            setTheme(android.R.style.Theme_DeviceDefault_DayNight);
-        }
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Create the WebView
         chatWebView = findViewById(R.id.chatWebView);
         registerForContextMenu(chatWebView);
 
-        //Set cookie options
         chatCookieManager = CookieManager.getInstance();
         chatCookieManager.setAcceptCookie(true);
         chatCookieManager.setAcceptThirdPartyCookies(chatWebView, false);
 
-        chatWebView.setWebChromeClient(new WebChromeClient(){
+        chatWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-                if (consoleMessage.message().contains("NotAllowedError: Write permission denied.")) {  //this error occurs when user copies to clipboard
-                    Toast.makeText(context, R.string.error_copy,Toast.LENGTH_LONG).show();
+                if (consoleMessage.message().contains("NotAllowedError: Write permission denied.")) {
+                    Toast.makeText(context, R.string.error_copy, Toast.LENGTH_LONG).show();
                     return true;
                 }
                 return false;
@@ -102,17 +62,14 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent, FILE_CHOOSER_REQUEST_CODE);
                 return true;
             }
-        });  //needed to share link
+        });
 
         chatWebView.setWebViewClient(new WebViewClient());
 
-        //Set more options
         WebSettings chatWebSettings = chatWebView.getSettings();
-        //Enable some WebView features
         chatWebSettings.setJavaScriptEnabled(true);
         chatWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
         chatWebSettings.setDomStorageEnabled(true);
-        //Disable some WebView features
         chatWebSettings.setAllowContentAccess(false);
         chatWebSettings.setAllowFileAccess(false);
         chatWebSettings.setBuiltInZoomControls(false);
@@ -121,25 +78,17 @@ public class MainActivity extends Activity {
         chatWebSettings.setSaveFormData(false);
         chatWebSettings.setGeolocationEnabled(false);
 
-        //Change the User-Agent
         chatWebSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 12; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36");
 
-        //Load Phind
         String urlToLoad = "https://phind.com/";
         chatWebView.loadUrl(urlToLoad);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        //Credit (CC BY-SA 3.0): https://stackoverflow.com/a/6077173
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             if (keyCode == KeyEvent.KEYCODE_BACK) {
-                if (chatWebView.canGoBack() && !Objects.equals(chatWebView.getUrl(), "about:blank")) {
+                if (chatWebView.canGoBack()) {
                     chatWebView.goBack();
                 } else {
                     finish();
@@ -151,7 +100,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         if (requestCode == FILE_CHOOSER_REQUEST_CODE) {
             if (mUploadMessage == null) return;
@@ -170,39 +119,28 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         WebView.HitTestResult result = chatWebView.getHitTestResult();
-        String url="";
+        String url = "";
         if (result.getExtra() != null) {
-            if (result.getType() == SRC_IMAGE_ANCHOR_TYPE) {
-                // Create a background thread that has a Looper
-                HandlerThread handlerThread = new HandlerThread("HandlerThread");
-                handlerThread.start();
-                // Create a handler to execute tasks in the background thread.
-                Handler backgroundHandler = new Handler(handlerThread.getLooper());
-                Message msg = backgroundHandler.obtainMessage();
-                chatWebView.requestFocusNodeHref(msg);
-                url = (String) msg.getData().get("src");
-            }  else if (result.getType() == IMAGE_TYPE) {
+            if (result.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE || result.getType() == WebView.HitTestResult.IMAGE_TYPE) {
                 url = result.getExtra();
-            }
-            if (url != null) {
-                Toast.makeText(this,getString(R.string.downloading),Toast.LENGTH_LONG).show();
-                String filename = URLUtil.guessFileName(url, null, null);
-                Uri source = Uri.parse(url);
-                DownloadManager.Request request = new DownloadManager.Request(source);
-                request.addRequestHeader("Cookie", CookieManager.getInstance().getCookie(url));
-                request.addRequestHeader("Accept", "text/html, application/xhtml+xml, *" + "/" + "*");
-                request.addRequestHeader("Accept-Language", "en-US,en;q=0.7,he;q=0.3");
-                request.addRequestHeader("Referer", url);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                assert dm != null;
-                dm.enqueue(request);
+                if (url != null) {
+                    Toast.makeText(this, getString(R.string.downloading), Toast.LENGTH_LONG).show();
+                    String filename = URLUtil.guessFileName(url, null, null);
+                    Uri source = Uri.parse(url);
+                    DownloadManager.Request request = new DownloadManager.Request(source);
+                    request.addRequestHeader("Cookie", chatCookieManager.getCookie(url));
+                    request.addRequestHeader("Accept", "text/html, application/xhtml+xml, */*");
+                    request.addRequestHeader("Accept-Language", "en-US,en;q=0.7,he;q=0.3");
+                    request.addRequestHeader("Referer", url);
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    dm.enqueue(request);
+                }
             }
         }
     }
-
 }
